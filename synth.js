85289421019cccd07d2774ef0c89a465bf7e93b6
Kostya <kurinniy.k@gmail.com>
2020-03-02 13:29:27 +0200

    scales and effects started

--- synth.js
+++ synth.js
@@ -1,6 +1,7 @@
 const container = document.getElementsByClassName('container')[0];
 const buttons = Array.from(document.getElementsByClassName('button'));
 const noteCountText = document.getElementById('notecount');
+const delayRange = document.getElementById('delay');
 
 const audioContext = new (window.AudioContext || window.webkitAudioContext);
 const oscList = [];
@@ -8,28 +9,41 @@ let masterGainNode = null;
 masterGainNode = audioContext.createGain();
 masterGainNode.connect(audioContext.destination);
 masterGainNode.gain.value = 0.2;
+const keys =
+  ["z", "s", "x", "d", "c", "v", "g", "b", "h", "n", "j", "m",
+    "q", "2", "w", "3", "e", "r", "5", "t", "6", "y", "7", "u",
+    "i", "9", "o", "0", "p", "-", "[", "=", "]"
+  ];
 let osc = null;
 let oscs = {};
-const waveTypes = ['sine', 'square', 'sawtooth', 'triangle' ];
+const waveTypes = ['sine', 'square', 'sawtooth', 'triangle'];
 let waveType = 'sine';
-let isDelayOn = false;
-let delayNode;
-function DelayToggle (e) {
-  console.log('enter');
+let isDelayOn = true;
+let delayNode = audioContext.createDelay(5);
 
-  if (e.keyCode === 13)  {
+const effectsNode = [{node: delayNode, on: isDelayOn}];
+
+function DelayToggle(e) {
+  if (e.keyCode === 13) {
     isDelayOn = !isDelayOn;
+    console.log(isDelayOn);
     if (isDelayOn) {
-      delayNode = audioContext.createDelay(1);
-    } else {
+      effectsNode[0].on = true;
+      //effectsNode[0].delayTime = parseFloat(delayRange.value/1000);
 
+    } else {
+      effectsNode[0].false = true;
     }
   }
 };
 
-document.body.addEventListener('onkeypress', () => {console.log('ahuet`')});
 
-function changeWavetype (type) {
+document.addEventListener('keypress', (e) => {
+  DelayToggle(e)
+});
+
+
+function changeWavetype(type) {
   waveType = type;
 }
 
@@ -42,20 +56,77 @@ function onMIDISuccess(midiAccess) {
 
 }
 
+
+const notesPlayed = {};
+
+function getMIDIMessage(midiMessage) {
+  const command = midiMessage.data[0];
+  const note = midiMessage.data[1];
+  const velocity = (midiMessage.data.length > 2) ? midiMessage.data[2] : 0; // a velocity value might not be included with a noteOff command
+  const noteShift = -49;
+  switch (command) {
+    case 144: // noteOn
+      if (velocity > 0) {
+        notesPlayed[note] = true;
+        noteOn(note + noteShift, velocity);
+      } else {
+        noteOff(note + noteShift);
+      }
+      break;
+    case 128: // noteOff
+      noteOff(note + noteShift);
+      break;
+  }
+}
+
+
+const createScale = (octaveMultiplier, intervals, tunedTo = 220) => {
+  let scale = [];
+  if (typeof intervals === 'number') {
+    for (let i = 0; i <= 88; i++) {
+      let freq = ((octaveMultiplier) ** ((i) / intervals)) * tunedTo;
+      scale.push(freq)
+    }
+  }
+  return scale;
+};
+
+const bpScale = createScale(3, 13);
+const eq12Scale = createScale(2, 12);
+let Scale = bpScale;
 noteOn = (note, velocity) => {
   if (osc) {
     osc.stop();
     osc = null;
   }
-  if (oscs[note]) {return}
+  if (oscs[note]) {
+    return
+  }
   let newOsc = audioContext.createOscillator();
   newOsc.type = waveType;
   oscs[note] = newOsc;
   newOsc.connect(masterGainNode);
-  let value = 10*(note-48);
+  let tempNode = newOsc ;
+  effectsNode.forEach((data) => {
+    if (data.on) {
+      tempNode.connect(data.node);
+      tempNode = data.node;
+    }
+  });
+  console.log(effectsNode);
+  tempNode.connect(masterGainNode);
+  let delayVal = parseFloat(delayRange.value/1000);
+  effectsNode[0].node.delayTime.setValueAtTime(delayVal, audioContext.currentTime);
+  console.log(audioContext.currentTime, audioContext.currentTime+delayVal);
+  effectsNode[0].node.delayTime.setValueAtTime(delayVal, audioContext.currentTime+delayVal);
+
+  //let value = 10*(note-48);
+  let noteIndex = note;
+  let value = noteIndex < Scale.length ? Scale[noteIndex] : 0;
+  console.log(value);
   noteCountText.innerHTML = `${note}/${value}`;
 
-  newOsc.frequency.value = parseFloat(50+value);
+  newOsc.frequency.value = parseFloat(value);
   masterGainNode.gain.value = velocity / 150;
   newOsc.start(0);
 };
@@ -63,45 +134,24 @@ noteOff = (note) => {
   if (oscs[note]) {
     oscs[note].stop();
     delete oscs[note];
-  };
- // masterGainNode.gain.value = 0;
+  }
+  // masterGainNode.gain.value = 0;
 };
 
-const notesPlayed = {};
-function getMIDIMessage(midiMessage) {
-  var command = midiMessage.data[0];
-  var note = midiMessage.data[1];
-  var velocity = (midiMessage.data.length > 2) ? midiMessage.data[2] : 0; // a velocity value might not be included with a noteOff command
-
-  switch (command) {
-    case 144: // noteOn
-      if (velocity > 0) {
-        notesPlayed[note] = true;
-        noteOn(note, velocity);
-      } else {
-        noteOff(note);
-      }
-      break;
-    case 128: // noteOff
-      noteOff(note);
-      break;
-  }
-}
-
-
-function playFreq (i) {
+function playFreq(i) {
   return function () {
     console.log('start');
     console.log(oscList[i]);
 
     oscList[i] = audioContext.createOscillator();
     oscList[i].connect(masterGainNode);
-    oscList[i].frequency.value = parseFloat(220 + 220 * i);
+    oscList[i].frequency.value = parseFloat(220 + 10 * i);
     oscList[i].start(0);
 
   }
 };
-function stopOsc (i) {
+
+function stopOsc(i) {
 
   return function () {
     console.log('stop');
@@ -109,7 +159,17 @@ function stopOsc (i) {
     oscList[i].stop(0);
   }
 };
+
 buttons.forEach(function (b, i) {
   b.addEventListener('mousedown', playFreq(i));
-  b.addEventListener("mouseup",stopOsc(i));
+  b.addEventListener("mouseup", stopOsc(i));
+});
+
+document.addEventListener('keypress', (event) => {
+  const keyIndex = keys.indexOf(event.key);
+  if (keyIndex >= 0) noteOn(keyIndex, 100);
+});
+document.addEventListener('keyup', (event) => {
+  const keyName = event.key;
+  noteOff(keys.indexOf(keyName));
 });
